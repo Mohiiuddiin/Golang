a4e49f99565c9585ae690cb443df6f2d1e9ca3de
Mohiiuddiin <45856100+Mohiiuddiin@users.noreply.github.com>
2021-03-04 17:47:23 +0600

    Update main.go

--- Count_The_Num/main.go
+++ Count_The_Num/main.go
@@ -19,3 +19,30 @@ func nbDig(n int, d int) int {
 	}
 	return count
 }
+// func NbDig(n int, d int) int {
+//     if n == 0 {
+//         return 0
+//     }
+    
+//     count := 0
+//     for i := 0; i <= n; i++ {
+//         count += digits(i*i, d)
+//     }
+//     return count
+// }
+
+// func digits(val, d int) int {
+//     if val == d {
+//         return 1
+//     }
+
+//     count := 0
+//     for val != 0 {
+//         digit := val % 10
+//         if digit == d {
+//           count++
+//         }
+//         val /= 10
+//     }
+//     return count
+// }
